2217c5bbcc70f32dfe4450e42bcf689329957059
tossudev <tossudev@gmail.com>
2026-01-26 13:37:47 +0200

    text rotation

--- elements/text.lua
+++ elements/text.lua
@@ -2,12 +2,14 @@ Text = Object:extend()
 require "utils"
 
 local scaleChange = 1.2
+local rotationLimit = 15.0 --random rotation value maximum (in degrees)
 
 
 function Text:new(text, x, y)
 	self.text = love.graphics.newText(Font, text)
 	self.x = x
 	self.y = y
+	self.r = 0.0
 
 	self:updateWH()	
 	self.sx = 1.0
@@ -15,17 +17,21 @@ function Text:new(text, x, y)
 
 	self.sxd = 1.0
 	self.syd = 1.0
+	self.rd = 0.0
 	self.vx = 1.0
 	self.vy = 1.0
+	self.vr = 1.0
 end
 
 
 function Text:update(dt)
 	self.sxd = lerp(self.sxd, 1.0, 0.5)
 	self.syd = lerp(self.syd, 1.0, 0.5)
+	self.rd = lerp(self.rd, 0.0, 0.2)
 	
 	self.sx, self.vx = spring(self.sx, self.vx, self.sxd, 0.2, 0.5)
 	self.sy, self.vy = spring(self.sy, self.vy, self.syd, 0.2, 0.2)
+	self.r = self.rd
 end
 
 
@@ -34,7 +40,7 @@ function Text:draw()
 		self.text,
 		self.x,
 		self.y,
-		0,
+		self.r,
 		self.sx,
 		self.sy,
 		self.ox,
@@ -55,6 +61,8 @@ function Text:updateText(newText)
 	self.text:setf(newText, 10000, "left")
 	self.sxd = scaleChange
 	self.syd = scaleChange
+	self.rd = math.random(-rotationLimit, rotationLimit)
+	self.rd = self.rd/57.29 --deg to rad
 
 	self:updateWH()
 end
